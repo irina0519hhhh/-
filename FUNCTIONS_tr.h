a6a00cb395aebf4e49279a6ec06b5c0b3153c5cb
Irina Dyachkova <159597927+irina0519hhhh@users.noreply.github.com>
2024-03-09 12:53:54 +1000

    Update FUNCTIONS_tr.h

--- FUNCTIONS_tr.h
+++ FUNCTIONS_tr.h
@@ -7,3 +7,22 @@ double per_tr (double a, double b, double c)
   }
   return a + b + c;
 }
+
+
+// Функция для вычисления площади треугольника
+
+#include <math.h>
+
+ double s; 
+ double sq_tr (double per_tr, double a, double b, double c)
+ {
+    if (a < 0 || b < 0 || c < 0)
+    {
+        return 0;
+    }
+    p = per_tr/2;
+    s = sqrt(p*(p-a)*(p-b)*(p-c));
+    return s;
+ } 
+
+
