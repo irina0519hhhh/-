03cd69c3c6dc482b4c15e6a12c7fd5cf179cd24c
Irina Dyachkova <159597927+irina0519hhhh@users.noreply.github.com>
2024-03-09 12:54:08 +1000

    Update FUNCTIONS_tr.h

--- FUNCTIONS_tr.h
+++ FUNCTIONS_tr.h
@@ -26,3 +26,11 @@ double per_tr (double a, double b, double c)
  } 
 
 
+// Функция для проверки неравенства треугольника
+int validate_triangle(double side_a, double side_b, double side_c) {
+if (side_a + side_b > side_c && side_a + side_c > side_b && side_b + side_c > side_a) {
+return 1;
+} else {
+return 0;
+}
+}
